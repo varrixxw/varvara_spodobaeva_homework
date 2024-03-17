e8e639514fc9bde81d80dc89837b806636efcfda
varya <spod.varvara26@icloud.com>
2024-03-18 00:31:31 +0300

    del

---
+++ homework4/task_5.py
@@ -0,0 +1,13 @@
+# Пользователь вводит три числа. Если все числа больше 10, то вывести на экран yes, иначе no.
+
+number1 = int(input('Введите 1 число:'))
+number2 = int(input('Введите 2 число:'))
+number3 = int(input('Введите 3 число:'))
+if number1 > 10 and number2 > 10 and number3 > 10:
+    print("yes")
+else:
+    print("no")
+
+
+
+
